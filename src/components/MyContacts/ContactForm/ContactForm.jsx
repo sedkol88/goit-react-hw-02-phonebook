d8f7e1c69bad52d213ea7aec59f3185341d29bb7
Andrew Butenko <sedkol88@gmail.com>
2024-01-12 22:52:11 +0200

    upgrade patterns in inputs

--- src/components/MyContacts/ContactForm/ContactForm.jsx
+++ src/components/MyContacts/ContactForm/ContactForm.jsx
@@ -42,7 +42,7 @@ class ContactForm extends Component {
             className={styles.inputName}
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' \\-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -54,7 +54,7 @@ class ContactForm extends Component {
             className={styles.inputTel}
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
